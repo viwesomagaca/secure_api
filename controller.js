35c99c7e5b2122aad75d5b5a9553de967b6ba55b
Viwe Somagaca <viwe@fonk.capetown>
2019-04-16 15:08:52 +0200

    Update item route complete

--- controller.js
+++ controller.js
@@ -1,37 +1,65 @@
  const storage = require('./model/item');
 
- const getItems = (req, res)=>{
-   storage.find()
-  .then(data =>{
-      console.log("data",data);
-      return res.status(200).json({ sucess: true, items: data})
-  }).catch(err =>{
-      return res.status(400).json({ sucess: false, error:err})
-  })
+ const getItems = (req, res) => {
+     storage.find()
+         .then(data => {
+             console.log("data", data);
+             return res.status(200).json({
+                 success: true,
+                 items: data
+             })
+         }).catch(err => {
+             return res.status(400).json({
+                 success: false,
+                 error: err
+             })
+         })
  };
 
- const addItems =(req, res)=>{
-  const Item = new storage();
-   Item.itemname = req.body.itemname
-   Item.description = req.body.description
+ const addItems = (req, res) => {
+     const Item = new storage();
+     Item.itemname = req.body.itemname
+     Item.description = req.body.description
 
-   Item.save(err =>{
-       res.status(200).json({ sucess:true})
-   })
+     Item.save(err => {
+         res.status(200).json({
+             success: true
+         })
+     })
  };
 
-const getItemById = (req,res)=>{
- const {id} = req.params;
-    storage.find({_id:id})
+ const getItemById = (req, res) => {
+  const {id} = req.params;
+     storage.find({  _id: id
+         })
+         .then(data => {
+             res.status(200).json({
+                 success: true,
+                 items: data
+             })
+         }).catch(err => {
+             res.status(400).json({
+                 success: false,
+                 error: err
+             });
+         })
+ }
+
+ const updateItem = (req,res)=>{
+    const {id} = req.params;
+    const {itemname, description} = req.body;
+    storage.findByIdAndUpdate(id, {itemname, description})
     .then(data =>{
-        res.status(200).json({sucess:true, items:data})
+        return res.status(200).json({ success: true});
     }).catch(err =>{
-        res.status(400).json({sucess:false, error:err});
+        return res.status(400).json({ success: false});
     })
-} 
- module.exports ={
+ }
+
+ module.exports = {
      getItems,
      addItems,
-     getItemById
-    
+     getItemById,
+     updateItem
+
  }
