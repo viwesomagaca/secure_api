3496a0237a7a97c236202b298389715dffe1013e
Viwe Somagaca <viwe@fonk.capetown>
2019-04-16 12:21:41 +0200

    Initial commit, Started server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT;
+const mongo_url = process.env.MONGO_URL;
+
+mongoose.connect(
+    mongo_url, 
+    { useNewUrlParser:true }
+  );
+
+let db = mongoose.connection;
+db.once('open', ()=> console.log('Connection to database is established'));
+
+db.on('error', console.error.bind(console, "Database connection error"));
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+mongoose.set('useFindOneAndUpdate', false);
+
+app.listen(port,() => console.log(`App listening on port ${port}!`))
